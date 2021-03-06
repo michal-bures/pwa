7de42d19ea49987527dc189b5434f073badc0e65
michal-bures <embaircz@gmail.com>
2021-03-06 11:46:38 +0100

    working version of minimal counter app

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# pwa
+# Progressive Web App Demo
+
+
+
+## Minimal progressive web app
+
+A minimal example (`./minimal`)
+ * no Webpack, no Npm, no Workbox, just html5 and plain javascript
+
+
+
+
+## Service workers
+
+### Limitations
+ no access to local storage and DOM
--- minimal/sw.js
+++ minimal/sw.js
@@ -1,17 +1,10 @@
 console.log('Script loaded!')
 
 const VERSION = 5
-
-const COUNTER_URL = 'https://api.countapi.xyz'
-const COUNTER_GET_URL = `${COUNTER_URL}/get`
-const COUNTER_UPDATE_URL = `${COUNTER_URL}/update`
-
-const LSKEY_COUNTER = 'counter-value'
-const LSKEY_DELTA = 'counter-delta'
-
 const CACHE_STORAGE_KEY = `pwa-counter-v${VERSION}`
 
-const cacheList = [
+// list of resources that will be cached client-side and served from the cache instead of server
+const cacheFirstList = [
     "index.html",
     "style.css",
 ]
@@ -33,44 +26,31 @@ self.addEventListener('message', (event) => {
 })
 
 self.addEventListener('fetch', function(e) {
-    const req = e.request
-    console.log('Fetch event:', req.url)
-
-    if (req.url.startsWith(COUNTER_URL)) {
-        try {
-            e.respondWith(fetch(req))
-        } catch (err) {
-            e.respondWith(handleCounterRequestWhileOffline(req))
-        }
-    } else {
-        e.respondWith(
-            caches.match(e.request).then((response) => {
-                if (response != null) {
-                    console.log('Using cache for:', e.request.url)
-                    return response
-                }
-                console.log('Fallback to fetch:', e.request.url)
-                return fetch(e.request)
-            })
-        )
-    }
+    e.respondWith(cacheFirst(e.request))
 })
+
+async function cacheFirst(request) {
+    //cache-first
+    const cachedResponse = await caches.match(request)
+    if (cachedResponse != null) {
+        console.log('Using cache for:', request.url)
+        return cachedResponse
+    } else {
+        console.log('Fallback to fetch:', request.url)
+        return fetch(request)
+    }
+}
 
 async function install() {
     const cache = await caches.open(CACHE_STORAGE_KEY)
-    console.log('Adding to Cache:', cacheList)
-    await cache.addAll(cacheList)
+    console.log('Adding to Cache:', cacheFirstList)
+    await cache.addAll(cacheFirstList)
     await self.skipWaiting()
 }
 
 
 async function activate() {
-    const cacheNames = await caches.keys()
-    await Promise.all(cacheNames.map(name => {
-        if (name !== CACHE_STORAGE_KEY) {
-            return caches.delete(name)
-        }
-    }))
+    await clearOldCaches()
     await self.clients.claim()
     const clients = await self.clients.matchAll()
     for (const client of clients) {
@@ -78,40 +58,12 @@ async function activate() {
     }
 }
 
-function handleCounterRequestWhileOffline(request) {
-    if (request.url.startsWith(COUNTER_GET_URL)) {
-        return new Response(getLocalCounterValue(), { headers: { 'Content-Type': 'text/plain' }});
-    } else if (request.url.startsWith(COUNTER_UPDATE_URL)) {
-        const currentValue = getLocalCounterValue()
-        const currentDelta = getLocalDelta()
-        const requestUrl = new URL(request.url)
-        const newDelta = currentDelta + requestUrl.searchParams.get('value')
-        console.log(`current ${currentValue}, delta ${newDelta}`)
-        setLocalCounter(currentValue + newDelta)
-        setLocalDelta(newDelta)
-        return new Response()
-    } else {
-        throw new Error(`Counter request to url ${request.url} not supported while offline.`)
-    }
+async function clearOldCaches() {
+    const cacheNames = await caches.keys()
+    await Promise.all(cacheNames.map(name => {
+        if (name !== CACHE_STORAGE_KEY) {
+            return caches.delete(name)
+        }
+    }))
 }
 
-function getLocalCounterValue() {
-    let storedValue = localStorage.get(LSKEY_COUNTER)
-    let storedDelta = getLocalDelta()
-    if (isNaN(storedValue)) storedValue = 0
-    return { value: storedValue + storedDelta }
-}
-
-function getLocalDelta() {
-    let storedDelta = localStorage.get(LSKEY_DELTA)
-    if (isNaN(storedDelta)) storedDelta = 0
-    return storedDelta
-}
-
-function setLocalDelta(newDelta) {
-    localStorage.set(LSKEY_DELTA, newDelta.toString())
-}
-
-function setLocalCounter(newValue) {
-    localStorage.set(LSKEY_COUNTER, newValue.toString())
-}
